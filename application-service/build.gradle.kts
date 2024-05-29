0a30859b08062f469d5e0d48fbb834255d2e0452
vlekic02 <vlekic@griddynamics.com>
2024-05-29 12:17:20 +0200

    GM-18 add dependencies

--- application-service/build.gradle.kts
+++ application-service/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "com.griddynamics"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
@@ -12,6 +18,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.test {
