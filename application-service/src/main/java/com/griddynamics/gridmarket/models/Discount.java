fa9b885aebcb2cd629e3ad0ac03792d1cad9d640
vlekic02 <vlekic@griddynamics.com>
2024-05-31 10:31:41 +0200

    GM-18 use static factory method instead of constructor

--- application-service/src/main/java/com/griddynamics/gridmarket/models/Discount.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/models/Discount.java
@@ -3,17 +3,56 @@ package com.griddynamics.gridmarket.models;
 import java.time.LocalDateTime;
 import java.util.function.DoubleBinaryOperator;
 
-public record Discount(
-    long id,
-    String name,
-    Type type,
-    double value,
-    LocalDateTime startTime,
-    LocalDateTime endTime
-) {
+public class Discount {
 
-  public Discount(long id, String name, Type type, double value) {
-    this(id, name, type, value, null, null);
+  private final long id;
+  private final String name;
+  private final Type type;
+  private final double value;
+  private final LocalDateTime startTime;
+  private final LocalDateTime endTime;
+
+  private Discount(long id, String name, Type type, double value, LocalDateTime startTime,
+      LocalDateTime endTime) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.value = value;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  public static Discount unlimited(long id, String name, Type type, double value) {
+    return new Discount(id, name, type, value, null, null);
+  }
+
+  public static Discount withTimeFrame(long id, String name, Type type, double value,
+      LocalDateTime startTime, LocalDateTime endTime) {
+    return new Discount(id, name, type, value, startTime, endTime);
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public double getValue() {
+    return value;
+  }
+
+  public LocalDateTime getStartTime() {
+    return startTime;
+  }
+
+  public LocalDateTime getEndTime() {
+    return endTime;
   }
 
   public double getRealPrice(double price) {
