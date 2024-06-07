98fff4c9400b728c958fe0d5b04bd227bd4dbd13
vlekic02 <vlekic@griddynamics.com>
2024-06-07 13:53:53 +0200

    GM-19 add application info

--- application-service/src/main/java/com/griddynamics/gridmarket/configuration/SwaggerConfiguration.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/configuration/SwaggerConfiguration.java
@@ -2,6 +2,8 @@ package com.griddynamics.gridmarket.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,17 @@ public class SwaggerConfiguration {
   @Bean
   public ModelResolver modelResolver(ObjectMapper objectMapper) {
     return new ModelResolver(objectMapper);
+  }
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(apiInfo());
+  }
+
+  private Info apiInfo() {
+    return new Info()
+        .title("GridMarket")
+        .description("E-commerce for applications");
   }
 }
