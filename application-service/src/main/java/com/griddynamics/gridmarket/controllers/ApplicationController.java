4c0d734ffab614bf4aa8fc8f5819fb17486f83e4
vlekic02 <vlekic@griddynamics.com>
2024-06-07 13:53:42 +0200

    GM-19 Update ApplicationController.java

--- application-service/src/main/java/com/griddynamics/gridmarket/controllers/ApplicationController.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/controllers/ApplicationController.java
@@ -33,8 +33,8 @@ public class ApplicationController {
   }
 
   @Operation(summary = "Get all reviews for specific application")
-  @GetMapping(value = "/{applicationId}/reviews", produces = "application/vnd.api+json")
-  public Collection<Review> getReviewByApplication(@PathVariable long applicationId) {
-    return applicationService.getAllReviewForApplication(applicationId);
+  @GetMapping(value = "/{id}/reviews", produces = "application/vnd.api+json")
+  public Collection<Review> getReviewByApplication(@PathVariable long id) {
+    return applicationService.getAllReviewForApplication(id);
   }
 }
