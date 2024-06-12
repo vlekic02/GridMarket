8bd593d13c0fdde8f075833580aa7e21cea92f27
vlekic02 <vlekic@griddynamics.com>
2024-06-12 11:07:22 +0200

    GM-29 simplify application row mapper

--- application-service/src/main/java/com/griddynamics/gridmarket/mappers/ApplicationRowMapper.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/mappers/ApplicationRowMapper.java
@@ -4,38 +4,32 @@ import com.griddynamics.gridmarket.models.Application;
 import com.griddynamics.gridmarket.models.Discount;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.lang.NonNull;
 
 public class ApplicationRowMapper implements RowMapper<Application> {
 
+  private final DiscountRowMapper discountRowMapper;
+
+  public ApplicationRowMapper() {
+    this.discountRowMapper = new DiscountRowMapper();
+  }
+
   @Override
   public Application mapRow(@NonNull ResultSet resultSet, int rowNum) throws SQLException {
+    Application.Builder builder = new Application.Builder();
     long id = resultSet.getLong("application_id");
-    String name = resultSet.getString("name");
+    builder.setId(id);
     String description = resultSet.getString("description");
+    builder.setDescription(description);
     String path = resultSet.getString("path");
-    Discount discount = null;
-    long discountId = resultSet.getLong("discount_id");
-    if (!resultSet.wasNull()) {
-      String discountName = resultSet.getString("discount_name");
-      Discount.Type discountType = Discount.Type.valueOf(resultSet.getString("type"));
-      double value = resultSet.getDouble("value");
-      Timestamp startTimestamp = resultSet.getTimestamp("start_date");
-      LocalDateTime startTime = startTimestamp == null ? null : startTimestamp.toLocalDateTime();
-      Timestamp endTimestamp = resultSet.getTimestamp("end_date");
-      LocalDateTime endTime = endTimestamp == null ? null : endTimestamp.toLocalDateTime();
-      if (startTime == null || endTime == null) {
-        discount = Discount.unlimited(discountId, discountName, discountType, value);
-      } else {
-        discount = Discount.withTimeFrame(discountId, discountName, discountType, value, startTime,
-            endTime);
-      }
-    }
+    builder.setPath(path);
+    Discount discount = discountRowMapper.mapRow(resultSet, rowNum);
+    builder.setDiscount(discount);
     double price = resultSet.getDouble("price");
-    int publisherId = resultSet.getInt("publisher");
-    return new Application(id, name, description, path, discount, price, publisherId);
+    builder.setOriginalPrice(price);
+    long publisherId = resultSet.getLong("publisher");
+    builder.setPublisher(publisherId);
+    return builder.build();
   }
 }
