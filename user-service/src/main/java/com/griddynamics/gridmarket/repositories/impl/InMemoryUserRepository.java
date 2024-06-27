d80c4c1cc8fc0dc5600510fd945264cca6675065
vlekic02 <vlekic@griddynamics.com>
2024-06-27 12:22:56 +0200

    GM-37 Update InMemoryUserRepository.java

--- user-service/src/main/java/com/griddynamics/gridmarket/repositories/impl/InMemoryUserRepository.java
+++ user-service/src/main/java/com/griddynamics/gridmarket/repositories/impl/InMemoryUserRepository.java
@@ -17,16 +17,15 @@ public class InMemoryUserRepository implements UserRepository {
   public InMemoryUserRepository() {
     Role adminRole = new Role(1, "ADMIN");
     Role memberRole = new Role(2, "MEMBER");
-    User admin = new User(1, "Ilija", "Mirkovic", "imirkovic", adminRole,
-        null, 1500);
     this.users = List.of(
-        admin,
+        new User(1, "Ilija", "Mirkovic", "imirkovic", adminRole,
+            null, 1500),
         new User(2, "Nemanja", "Mijatovic", "nmijatovic", memberRole,
             null, 150.3),
         new User(3, "Marko", "Stojkovic", "mstojkovic", memberRole,
             null, 5.5),
         new User(4, "Uros", "Rankovic", "urankovic", memberRole,
-            new Ban(1, admin, LocalDateTime.now(), "fraud"), 3)
+            new Ban(1, 1, LocalDateTime.now(), "fraud"), 3)
     );
   }
 
