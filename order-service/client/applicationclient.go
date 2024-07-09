8947da9aa0729207f4b20fd611e30b43374e626c
vlekic02 <vlekic@griddynamics.com>
2024-07-09 11:07:04 +0200

    GM-52 Create applicationclient.go

---
+++ order-service/client/applicationclient.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	log "order-service/logging"
+	"order-service/model"
+)
+
+type ApplicationPriceResponse struct {
+	Data struct {
+		Id         string `json:"id"`
+		Type       string `json:"type"`
+		Attributes struct {
+			Price float64 `json:"price"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
+
+func GetApplicationPrice(id uint64) (ApplicationPriceResponse, error) {
+	log.Debug(fmt.Sprintf("Calling application service /internal/%d/price", id))
+	response, err := http.Get(fmt.Sprintf("http://application-service:8080/internal/%d/price", id))
+	applicationResponse := ApplicationPriceResponse{}
+	if err != nil {
+		return applicationResponse, &model.RestError{Title: "Gateway Timeout", Status: 504, Detail: "Application service did not respond ! Error: " + err.Error()}
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return applicationResponse, &model.RestError{Title: "Internal Server Error", Status: 500, Detail: "Application service response does not contains body ! Error: " + err.Error()}
+	}
+	if response.StatusCode != http.StatusOK {
+		errorResponse := new(model.ErrorsResponse)
+		if err := json.Unmarshal(body, &errorResponse); err != nil {
+			return applicationResponse, &model.RestError{Title: "Internal Server Error", Status: 500, Detail: "Failed to unmarshal application service response ! Error: " + err.Error()}
+		}
+		return applicationResponse, &errorResponse.Errors[0].Attributes
+	}
+	if err := json.Unmarshal(body, &applicationResponse); err != nil {
+		return applicationResponse, &model.RestError{Title: "Internal Server Error", Status: 500, Detail: "Failed to unmarshal application service response ! Error: " + err.Error()}
+	}
+	return applicationResponse, nil
+}
