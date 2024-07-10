dcda210e66fad31061ba55880055c4e7e7fe3d10
vlekic02 <vlekic@griddynamics.com>
2024-07-10 13:24:18 +0200

    GM-53 Create schema.sql

---
+++ order-service/schema.sql
@@ -0,0 +1,14 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_method') THEN
+        CREATE TYPE payment_method AS ENUM ('BALANCE', 'PAYPAL');
+    END IF;
+END$$@@
+
+CREATE TABLE IF NOT EXISTS "order" (
+    order_id SERIAL PRIMARY KEY,
+    "user" INTEGER NOT NULL,
+    application INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL,
+    method payment_method NOT NULL
+)
