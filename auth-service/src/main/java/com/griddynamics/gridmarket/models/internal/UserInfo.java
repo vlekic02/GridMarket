4b5585b5fb21f63dfd0dd702358949a4063c0bca
vlekic02 <vlekic@griddynamics.com>
2024-07-18 13:38:18 +0200

    GM-56 ignore claims when serializing

--- auth-service/src/main/java/com/griddynamics/gridmarket/models/internal/UserInfo.java
+++ auth-service/src/main/java/com/griddynamics/gridmarket/models/internal/UserInfo.java
@@ -1,10 +1,12 @@
 package com.griddynamics.gridmarket.models.internal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Map;
 
 public record UserInfo(long id, String name, String surname, String username, String role,
                        double balance) {
 
+  @JsonIgnore
   public Map<String, Object> getClaims() {
     return Map.of(
         "id", id,
