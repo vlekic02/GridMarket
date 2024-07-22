39becf1cb95c492291b6d86a539b823d373e06ea
vlekic02 <vlekic@griddynamics.com>
2024-07-22 13:59:10 +0200

    GM-57 create user info resolver & add it to webmvc

--- application-service/src/main/java/com/griddynamics/gridmarket/configuration/WebConfiguration.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/configuration/WebConfiguration.java
@@ -1,10 +1,14 @@
 package com.griddynamics.gridmarket.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.griddynamics.gridmarket.interceptors.PrometheusCounterInterceptor;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import java.util.List;
 import java.util.Map;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
   private final Map<String, Counter> prometheusCounters;
+  private final ObjectMapper objectMapper;
 
-  public WebConfiguration(MeterRegistry registry) {
+  public WebConfiguration(MeterRegistry registry, ObjectMapper objectMapper) {
     prometheusCounters = Map.of(
         "/:GET", Counter.builder("http_request_counter")
             .tags("uri", "/", "method", "GET")
@@ -26,10 +31,16 @@ public class WebConfiguration implements WebMvcConfigurer {
             .tags("uri", "/{id}/reviews", "method", "GET")
             .register(registry)
     );
+    this.objectMapper = objectMapper;
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new PrometheusCounterInterceptor(prometheusCounters));
+  }
+
+  @Override
+  public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new UserInfoResolver(objectMapper));
   }
 }
