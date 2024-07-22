ad3143e2c803fc8ebfabc317da44ea3f5fd2f53e
vlekic02 <vlekic@griddynamics.com>
2024-07-22 12:26:11 +0200

    GM-57 Update GridUserAuthenticationToken.java

--- gateway-service/src/main/java/com/griddynamics/gridmarket/token/GridUserAuthenticationToken.java
+++ gateway-service/src/main/java/com/griddynamics/gridmarket/token/GridUserAuthenticationToken.java
@@ -1,19 +1,16 @@
 package com.griddynamics.gridmarket.token;
 
 import com.griddynamics.gridmarket.model.GridUser;
-import java.util.Collection;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
 
-public class GridUserAuthenticationToken extends AbstractAuthenticationToken {
+public class GridUserAuthenticationToken extends BearerTokenAuthenticationToken {
 
   private final Jwt jwt;
   private final GridUser gridUser;
 
-  public GridUserAuthenticationToken(Jwt jwt, GridUser gridUser,
-      Collection<? extends GrantedAuthority> authorities) {
-    super(authorities);
+  public GridUserAuthenticationToken(Jwt jwt, GridUser gridUser) {
+    super(jwt.getTokenValue());
     this.jwt = jwt;
     this.gridUser = gridUser;
     setAuthenticated(true);
