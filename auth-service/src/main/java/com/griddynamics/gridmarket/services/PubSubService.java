9855cac1fbbdff03afc37c700c61c1d74545b610
vlekic02 <vlekic@griddynamics.com>
2024-07-23 22:43:28 +0200

    GM-10 Update PubSubService.java

--- auth-service/src/main/java/com/griddynamics/gridmarket/services/PubSubService.java
+++ auth-service/src/main/java/com/griddynamics/gridmarket/services/PubSubService.java
@@ -2,12 +2,15 @@ package com.griddynamics.gridmarket.services;
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.griddynamics.gridmarket.events.UserRegistrationEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PubSubService {
 
   private static final String USER_REGISTRATION_TOPIC = "user-registration";
+  private static final Logger LOGGER = LoggerFactory.getLogger(PubSubService.class);
 
   private final PubSubTemplate pubSubTemplate;
 
@@ -16,6 +19,7 @@ public class PubSubService {
   }
 
   public void publishUserRegistration(UserRegistrationEvent event) {
+    LOGGER.debug("Sending user registration event to pub/sub ! {}", event);
     pubSubTemplate.publish(USER_REGISTRATION_TOPIC, event);
   }
 
