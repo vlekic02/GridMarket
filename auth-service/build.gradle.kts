1557ffa508da69e674744d7616a968b7e82fa72b
vlekic02 <vlekic@griddynamics.com>
2024-07-23 14:07:19 +0200

    GM-10 configure pubsub emulator

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     jacoco
     checkstyle
-    id("org.springframework.boot") version "3.2.8"
+    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.sonarqube") version "5.0.0.4638"
 }
---
+++ scripts/pubsub-init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+main() {
+  gcloud beta emulators pubsub start --project=gridmarket-dev --host-port='0.0.0.0:8085' &
+  sleep 10
+  createTopics
+  tail -f /dev/null
+}
+
+createTopics() {
+  apt-get update
+  apt-get install python3-pip -y
+  cd pubsub/samples/snippets
+  pip install -r requirements.txt
+  export PUBSUB_EMULATOR_HOST=localhost:8085
+  python3 publisher.py gridmarket-dev create user-registration
+}
+
+main "$@"
