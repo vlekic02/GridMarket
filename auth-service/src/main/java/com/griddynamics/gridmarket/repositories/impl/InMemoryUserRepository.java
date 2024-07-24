2d7ff58a11b478900b8f6e1906b618088b1e5fa2
vlekic02 <vlekic@griddynamics.com>
2024-07-25 00:24:52 +0200

    GM-10 implement inmemory repo

--- auth-service/src/main/java/com/griddynamics/gridmarket/repositories/impl/InMemoryUserRepository.java
+++ auth-service/src/main/java/com/griddynamics/gridmarket/repositories/impl/InMemoryUserRepository.java
@@ -7,12 +7,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class InMemoryUserRepository implements UserRepository {
 
   private final Map<String, User> usersMap;
+  private final PasswordEncoder passwordEncoder;
+  private long lastId;
 
   public InMemoryUserRepository() {
+    this.passwordEncoder = new BCryptPasswordEncoder();
     usersMap = new HashMap<>();
     List<User> users = List.of(
         new User(
@@ -21,6 +26,7 @@ public class InMemoryUserRepository implements UserRepository {
             "$2a$12$HxWrdRqiBamt3NGyp7xoreXu2Ig7yVUbtySR1mfgrZSdYBQjOHniG"
         )
     );
+    lastId = 1;
     for (User user : users) {
       usersMap.put(user.getUsername().toLowerCase(), user);
     }
@@ -32,7 +38,9 @@ public class InMemoryUserRepository implements UserRepository {
   }
 
   @Override
-  public void registerUser(UserRegistrationRequest userRegistrationRequest) {
-
+  public void addRegisteredUser(UserRegistrationRequest request) {
+    usersMap.put(request.username().toLowerCase(),
+        new User(++lastId,
+            request.username(), passwordEncoder.encode(request.password())));
   }
 }
