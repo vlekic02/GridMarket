f0642300a7343dc5fa70114a1ec0d81a0742faf8
vlekic02 <vlekic@griddynamics.com>
2024-07-25 15:27:39 +0200

    GM-63 Update PubSubService.java

--- user-service/src/main/java/com/griddynamics/gridmarket/services/PubSubService.java
+++ user-service/src/main/java/com/griddynamics/gridmarket/services/PubSubService.java
@@ -2,6 +2,7 @@ package com.griddynamics.gridmarket.services;
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.griddynamics.gridmarket.pubsub.event.UserDeletionEvent;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,8 @@ public class PubSubService {
   }
 
   public void publishUserDeletion(UserDeletionEvent event) {
-    //todo
+    LOGGER.debug("Publishing user deletion event to pub/sub ! {}", event);
+    template.publish(USER_TOPIC, event, Map.of("event", "user_deletion"))
+        .thenAccept(id -> LOGGER.debug("User deletion event published with id {} !", id));
   }
 }
