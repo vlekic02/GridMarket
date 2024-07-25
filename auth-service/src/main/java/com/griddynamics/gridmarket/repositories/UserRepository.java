3380205211d079ebfd2c85bf46133b67ff6e8054
vlekic02 <vlekic@griddynamics.com>
2024-07-25 12:09:02 +0200

    GM-10 shift encoding logic up the chain

--- auth-service/src/main/java/com/griddynamics/gridmarket/repositories/UserRepository.java
+++ auth-service/src/main/java/com/griddynamics/gridmarket/repositories/UserRepository.java
@@ -1,12 +1,11 @@
 package com.griddynamics.gridmarket.repositories;
 
 import com.griddynamics.gridmarket.models.User;
-import com.griddynamics.gridmarket.requests.UserRegistrationRequest;
 import java.util.Optional;
 
 public interface UserRepository {
 
   Optional<User> findByUsername(String username);
 
-  void addRegisteredUser(UserRegistrationRequest userRegistrationRequest);
+  void addRegisteredUser(String username, String encodedPassword);
 }
