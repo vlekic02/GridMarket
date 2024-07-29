8fbfcfb6163eed47efda0149a341156abb4e5db1
vlekic02 <vlekic@griddynamics.com>
2024-07-29 12:07:01 +0200

    GM-11 add controller

--- auth-service/src/main/java/com/griddynamics/gridmarket/controllers/AuthenticationController.java
+++ auth-service/src/main/java/com/griddynamics/gridmarket/controllers/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.griddynamics.gridmarket.controllers;
 
+import com.griddynamics.gridmarket.requests.ChangePasswordRequest;
 import com.griddynamics.gridmarket.requests.UserRegistrationRequest;
 import com.griddynamics.gridmarket.services.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
+
+  private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
   private final UserService userService;
 
@@ -27,9 +32,19 @@ public class AuthenticationController {
     return "register-page";
   }
 
+  @GetMapping("/changepassword")
+  public String changePasswordPage() {
+    return "change-password";
+  }
+
   @PostMapping(value = "/register", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   public String registerUser(@Valid UserRegistrationRequest registrationRequest) {
     userService.registerUser(registrationRequest);
     return "redirect:register?success";
+  }
+
+  @PostMapping(value = "/changepassword", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public String changePassword(@Valid ChangePasswordRequest changePasswordRequest) {
+    return userService.changePassword(changePasswordRequest);
   }
 }
