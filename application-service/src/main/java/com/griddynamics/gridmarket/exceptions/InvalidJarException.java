aea4b747709fed3f979f0c1932ccf225750e8178
vlekic02 <vlekic@griddynamics.com>
2024-07-31 11:32:08 +0200

    GM-60 Delete InvalidJarException.java

--- application-service/src/main/java/com/griddynamics/gridmarket/exceptions/InvalidJarException.java
+++
@@ -1,8 +0,0 @@
-package com.griddynamics.gridmarket.exceptions;
-
-public class InvalidJarException extends BadRequestException {
-
-  public InvalidJarException() {
-    super("Provided file is not a valid jar file !");
-  }
-}
