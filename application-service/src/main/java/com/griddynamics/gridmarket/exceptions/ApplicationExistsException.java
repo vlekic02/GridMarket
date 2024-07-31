444d2d2937f959e3ffc18e21e9986a9eb3866ae1
vlekic02 <vlekic@griddynamics.com>
2024-07-31 10:51:39 +0200

    GM-60 add quotation marks

--- application-service/src/main/java/com/griddynamics/gridmarket/exceptions/ApplicationExistsException.java
+++ application-service/src/main/java/com/griddynamics/gridmarket/exceptions/ApplicationExistsException.java
@@ -3,6 +3,6 @@ package com.griddynamics.gridmarket.exceptions;
 public class ApplicationExistsException extends BadRequestException {
 
   public ApplicationExistsException(String name) {
-    super("Application with name " + name + " already exists");
+    super("Application with name \"" + name + "\" already exists");
   }
 }
