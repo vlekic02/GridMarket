44e85e50a52530ce188da592cdb2cd215a128b95
vlekic02 <vlekic@griddynamics.com>
2024-08-15 16:12:28 +0200

    GM-76 Update pubsub-init.sh

--- scripts/pubsub-init.sh
+++ scripts/pubsub-init.sh
@@ -26,6 +26,7 @@ createTopics() {
   pip install -r requirements.txt
   export PUBSUB_EMULATOR_HOST=localhost:8085
   python3 publisher.py gridmarket-dev create user
+  python3 publisher.py gridmarket-dev create order
 }
 
 createSubscriptions() {
