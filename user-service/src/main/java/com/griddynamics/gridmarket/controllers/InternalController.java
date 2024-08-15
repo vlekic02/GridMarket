13a72052f286da9292dd9dee351fe8724231f2e1
vlekic02 <vlekic@griddynamics.com>
2024-08-15 15:38:44 +0200

    GM-76 add simple handler

--- user-service/src/main/java/com/griddynamics/gridmarket/controllers/InternalController.java
+++ user-service/src/main/java/com/griddynamics/gridmarket/controllers/InternalController.java
@@ -1,10 +1,13 @@
 package com.griddynamics.gridmarket.controllers;
 
+import com.griddynamics.gridmarket.http.request.UserPayRequest;
 import com.griddynamics.gridmarket.models.internal.UserInternalDto;
 import com.griddynamics.gridmarket.services.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,10 @@ public class InternalController {
   @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
   public UserInternalDto getUserByUsername(@PathVariable String username) {
     return new UserInternalDto(userService.getUserByUsername(username));
+  }
+
+  @PostMapping(value = "/{id}/pay")
+  public void proceedPayment(@RequestBody UserPayRequest request) {
+    System.out.println(request);
   }
 }
---
+++ user-service/src/main/java/com/griddynamics/gridmarket/http/request/UserPayRequest.java
@@ -0,0 +1,5 @@
+package com.griddynamics.gridmarket.http.request;
+
+public record UserPayRequest(double amount) {
+
+}
