14d130c12f17cdbea2705817fb1fb1efe95034aa
vlekic02 <vlekic@griddynamics.com>
2024-08-16 14:02:19 +0200

    GM-76 Update userclient.go

--- order-service/client/userclient.go
+++ order-service/client/userclient.go
@@ -3,7 +3,6 @@ package client
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	log "order-service/logging"
 	"order-service/model"
@@ -13,25 +12,26 @@ type UserClient struct {
 	HttpClient
 }
 
-type UserPayRequest struct {
+type UserTransactionRequest struct {
+	Payer  int32   `json:"payer"`
+	Payee  int32   `json:"payee"`
 	Amount float64 `json:"amount"`
 }
 
 var DefaultUserClient = UserClient{HttpClient: http.DefaultClient}
 
-func (uc *UserClient) RemoveUserBalance(id int32, request UserPayRequest) error {
-	log.Debug(fmt.Sprintf("Calling user service /internal/users/%d/pay", id))
+func (uc *UserClient) MakeTransaction(request UserTransactionRequest) error {
+	log.Debug("Calling /internal/users/transaction with transaction data", "data", request)
 	buf, err := json.Marshal(&request)
 	if err != nil {
 		log.Error("Failed to marshal user pay request !", "error", err)
 		return model.NewRestError(500, "Internal Server Error", "Failed to marshal user service response ! Error: "+err.Error())
 	}
-	response, err := uc.Post(fmt.Sprintf("http://user-service:8080/internal/users/%d/pay", id), "application/json", bytes.NewReader(buf))
+	response, err := uc.Post("http://user-service:8080/internal/users/transaction", "application/json", bytes.NewReader(buf))
 	if err != nil {
 		return model.NewRestError(504, "Gateway Timeout", "Application service did not respond ! Error: "+err.Error())
 	}
 	defer response.Body.Close()
-	log.Debug("a", response.StatusCode)
 	if response.StatusCode == http.StatusOK {
 		return nil
 	}
